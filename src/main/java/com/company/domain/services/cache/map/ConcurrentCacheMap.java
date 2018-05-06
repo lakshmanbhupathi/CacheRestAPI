e752a3562a35bf0eda6baa446026165296e0726f
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-05-06 15:40:12 +0530

    master: Added ConcurrentCacheMap : Added remove()

--- src/main/java/com/company/domain/services/cache/map/ConcurrentCacheMap.java
+++ src/main/java/com/company/domain/services/cache/map/ConcurrentCacheMap.java
@@ -1,6 +1,7 @@
 package com.company.domain.services.cache.map;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -23,11 +24,12 @@ public class ConcurrentCacheMap<K, V> {
     private Entry<K, V> tail;
 
     private ConcurrentHashMap<K, Entry<K, V>> map;
-    private Lock lock;
+    private Lock lock = new ReentrantLock();
+    Condition isEmptyCondition = lock.newCondition();
+    Condition isFullCondition = lock.newCondition();
 
     ConcurrentCacheMap() {
         map = new ConcurrentHashMap<>();
-        lock = new ReentrantLock();
     }
 
     public boolean add(K key, V value) {
@@ -35,6 +37,7 @@ public class ConcurrentCacheMap<K, V> {
         if (head == null && tail == null && map.isEmpty()) {
             map.put(key, newEntry);
             head = tail = newEntry;
+            isEmptyCondition.signal();
             return true;
         }
 
@@ -46,11 +49,29 @@ public class ConcurrentCacheMap<K, V> {
         } finally {
             lock.unlock();
         }
-        if(map.putIfAbsent(key,newEntry) == null){
+        if (map.putIfAbsent(key, newEntry) == null) {
             return true;
         }
 
         return false;
+    }
+
+    public boolean remove(K key) {
+        if (map.contains(key)) {
+            try {
+                lock.lock();
+
+                Entry entry = map.get(key);
+                entry.prev.next = entry.next;
+                entry.next.prev = entry.prev;
+            } finally {
+                lock.unlock();
+            }
+            map.remove(key);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
